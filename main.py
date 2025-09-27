6a138cb2204304f5bd3c85379a628a8c164e79f3
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-09-27 15:33:22 -0400

    Implemented tab switching between the different William & Mary departments in the UI that will render the appropriate department's research cards.

--- main.py
+++ main.py
@@ -4,11 +4,19 @@ import json
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def cs():
     with open('static/cs_data.json') as file:
         cs_research_data = json.load(file)
-    return render_template('homepage.html', cs_research_data=cs_research_data)
+    return render_template('homepage.html', research_data=cs_research_data, active_tab='cs')
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/math')
+def math():
+    return render_template('homepage.html', research_data={}, active_tab='math')
+
+@app.route('/biology')
+def biology():
+    return render_template('homepage.html', research_data={}, active_tab='biology')
+
+@app.route('/english')
+def english():
+    return render_template('homepage.html', research_data={}, active_tab='english')
