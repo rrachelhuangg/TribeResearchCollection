d3ab0a38870d30c1d2b143138f69db3efc9b5d9a
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-09-27 15:15:10 -0400

    Passed the web-scraped data to GPT-4o for summarization. UI research project cards now display summarized text and are less cluttered.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('homepage.html')
+    with open('static/cs_data.json') as file:
+        cs_research_data = json.load(file)
+    return render_template('homepage.html', cs_research_data=cs_research_data)
 
 @app.route('/about')
 def about():
---
+++ src/summarize.py
@@ -0,0 +1,26 @@
+import json
+from openai import OpenAI
+
+client = OpenAI(
+    api_key="",
+)
+
+try:
+    with open("../static/cs_data.json") as file:
+        cs_data = json.load(file)
+    for entry in cs_data:
+        text_to_summarize = entry["description"]
+        prompt=f"Summarize the provided text in one concise sentence: {text_to_summarize}."
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "developer", "content": "You are an assistant that specializes in summarizing text. "},
+                {
+                    "role": "user",
+                    "content": prompt,
+                },
+            ],
+        )
+        print(f"SUMMARIZED TEXT: {completion.choices[0].message.content}\n")
+except Exception as e:
+    print(f"The following error occured: {e}")
